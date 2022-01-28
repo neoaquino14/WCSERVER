04e7e2ce2f2ab8f8c0d3acf76aaf43338c7c3818
neoaquino14 <98503313+neoaquino14@users.noreply.github.com>
2022-01-29 01:08:51 +0800

    Add files via upload

---
+++ Demos/demo-2.js
@@ -0,0 +1,13 @@
+//Neo Aquino
+//2075 - WCSERVER
+//Loading Core Modules
+
+var http = required('http');
+
+var server = http.createServer(function(req, res) {
+  
+  //path code here 
+
+});
+
+server.listen(5000);

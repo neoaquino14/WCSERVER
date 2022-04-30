c8144f270658d31be38a3e6ee7c944ee62e13105
Neo Aquino <98503313+neoaquino14@users.noreply.github.com>
2022-05-01 03:02:14 +0800

    Create App.vue

---
+++ finals-mevn/mevn-src/App.vue
@@ -0,0 +1,42 @@
+<!-- Author: Aquino, Neo
+     File: App.vue -->
+
+<template>
+  <div>
+    <!-- Nav bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand float-left">MEVN CRUD Application</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Create Student</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/view">View Students</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

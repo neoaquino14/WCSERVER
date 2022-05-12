c8bca2f4ccbf6fc339aad030908bd97c1834ff4b
Neo Aquino <98503313+neoaquino14@users.noreply.github.com>
2022-05-13 02:05:21 +0800

    Create About.vue

---
+++ wcserver_final_/src/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <body>
+        <img src="../assets/About.png" alt="" srcset="">
+        <div>
+            <h1>About Us</h1>
+            <p>Founded in 2022 by five university friends in Angeles City (Phillippines), Trip Now has since become a leading global accommodation search website. 
+                We are focused on reshaping the way millions of travelers search for and compare travel expenses, Trip Now’s mission is to become your companion to experience our world.</p>
+        </div>
+    </body>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        titleHome : "About"
+        
+        }
+    }
+}
+</script>
+<style scoped>
+img {
+    margin-top: -45px;
+    height: 100%;
+    width: 100%;
+  }
+div{
+    padding: 25px;
+    background-color: bisque;
+}
+</style>
